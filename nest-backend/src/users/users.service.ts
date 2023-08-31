80dd26f9ed337eae370da004649023de522c8749
KKardd <wlstjd4013@gmail.com>
2023-08-31 15:02:23 +0900

    feat: add basic create user method

--- nest-backend/src/users/users.service.ts
+++ nest-backend/src/users/users.service.ts
@@ -1,4 +1,28 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  async createUser(name: string, email: string, password: string) {
+    await this.checkUserExists(email);
+  }
+
+  private checkUserExists(email: string) {
+    return false; // TODO: DB연동 후 구현
+  }
+
+  private saveUser(
+    name: string,
+    email: string,
+    password: string,
+    signupVerifyToken: string,
+  ) {
+    return; // TODO: DB연동 후 구현
+  }
+
+  //   private async sendMemberJoinEmail(email: string, signupVerifyToken: string) {
+  //     await this.emailService.sendMemberJoinVerification(
+  //       email,
+  //       signupVerifyToken,
+  //     );
+  //   }
+}
