ce35197b2c2c0c1ece77ec215123bcaf972156a5
KKardd <wlstjd4013@gmail.com>
2023-09-03 14:34:36 +0900

    feat: apply env environments

--- nest-backend/src/app.module.ts
+++ nest-backend/src/app.module.ts
@@ -1,21 +1,18 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { UsersModule } from './users/users.module';
-import { EmailModule } from './email/email.module';
+import { validationSchema } from './config/validationSchema';
+import emailConfig from './config/emailConfig';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({
-      envFilePath:
-        process.env.NODE_ENV === 'production'
-          ? '.production.env'
-          : process.env.NODE_ENV === 'stage'
-          ? '.stage.env'
-          : '.development.env',
-      isGlobal: true,
-    }),
     UsersModule,
-    EmailModule,
+    ConfigModule.forRoot({
+      envFilePath: [`${__dirname}/config/env/.${process.env.NODE_ENV}.env`],
+      load: [emailConfig],
+      isGlobal: true,
+      validationSchema,
+    }),
   ],
   controllers: [],
   providers: [],
