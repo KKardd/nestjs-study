8b47f3b03215704175a53fabfd6ce79a1da3dd4a
KKardd <wlstjd4013@gmail.com>
2023-09-04 16:34:24 +0900

    feat: apply not-in decorators about name to password

--- nest-backend/src/users/dto/create-user.dto.ts
+++ nest-backend/src/users/dto/create-user.dto.ts
@@ -6,8 +6,19 @@ import {
   MaxLength,
   MinLength,
 } from 'class-validator';
+import { NotIn } from 'src/utils/decorators/not-in';
 
 export class CreateUserDto {
+  // @Transform(({ value, obj }) => {
+  //   if (obj.password.includes(obj.name.trim())) {
+  //     throw new BadRequestException('password는 name과 같은 문자열을 포함할 수 없습니다.');
+  //   }
+  //   return value.trim();
+  // })
+  @Transform((params) => params.value.trim())
+  @NotIn('password', {
+    message: `password는 name과 같은 문자열을 포함하지 못합니다.`,
+  })
   @IsString()
   @MinLength(2)
   @MaxLength(30)
