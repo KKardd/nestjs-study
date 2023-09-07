fe8439bc42243915997b35b585d3a5f14e72cbb9
KKardd <wlstjd4013@gmail.com>
2023-09-08 01:39:25 +0900

    feat: declare to typeorm

---
+++ nest-backend/src/ormconfig.ts
@@ -0,0 +1,15 @@
+import { DataSource } from 'typeorm';
+
+export const AppDataSource = new DataSource({
+  type: 'mysql',
+  host: process.env.DATABASE_HOST,
+  port: 3306,
+  username: process.env.DATABASE_USERNAME,
+  password: process.env.DATABASE_PASSWORD,
+  database: 'test',
+  entities: [__dirname + '/**/*.entity{.ts,.js}'],
+  synchronize: false,
+  migrations: [__dirname + '/**/migrations/*.js'],
+  migrationsRun: false,
+  migrationsTableName: 'migrations',
+});
