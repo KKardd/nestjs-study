7ab8fcaf2fca1a9b0bd3216d51b0a799917a8fdf
KKardd <wlstjd4013@gmail.com>
2023-09-16 21:42:00 +0900

    feat: add configuration to auth

--- nest-backend/src/app.module.ts
+++ nest-backend/src/app.module.ts
@@ -3,14 +3,16 @@ import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModule } from './users/users.module';
 import { validationSchema } from './config/validationSchema';
+import { AuthModule } from './auth/auth.module';
 import emailConfig from './config/emailConfig';
+import authConfig from './config/authConfig';
 
 @Module({
   imports: [
     UsersModule,
     ConfigModule.forRoot({
       envFilePath: [`${__dirname}/config/env/.${process.env.NODE_ENV}.env`],
-      load: [emailConfig],
+      load: [emailConfig, authConfig],
       isGlobal: true,
       validationSchema,
     }),
@@ -22,11 +24,12 @@ import emailConfig from './config/emailConfig';
       password: process.env.DATABASE_PASSWORD,
       database: 'test',
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
-      synchronize: false,
+      synchronize: true,
       migrationsRun: false,
       migrations: [__dirname + '**/migrations/*.js'],
       migrationsTableName: 'migrations',
     }),
+    AuthModule,
   ],
   controllers: [],
   providers: [],
